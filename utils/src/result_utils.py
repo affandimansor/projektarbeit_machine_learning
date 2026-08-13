a7e1065a068011c64aa33dba1c11c2b4facfd1e2
affandimansor <affandimansor@gmail.com>
2026-08-13 12:52:04 +0200

    Defined CheckModelFit -To analyse the model fitting on train and test dataset

--- utils/src/result_utils.py
+++ utils/src/result_utils.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, accuracy_score
 from mlxtend.plotting import heatmap
 
 # Die Funktion erstelle den Confusion Matrix des Ergebnisses und visualisiert ihn auf einem Heatmap
@@ -17,3 +17,28 @@ def CreateVisualizeConfusionMatrix(y_test, y_predicted):
     plt.xlabel('Prediction')
     plt.ylabel('True')
     plt.show()
+
+# Die Funktion prueft das Fitting ein Modell anhand der accuracy scores (as)
+# Alle Eingabeparameter muessen in % uebergeben werden
+def CheckModelFit(as_train, as_test, underfit_threshold=None, overfit_threshold=None):
+    # Definiere den Defaultwert des underfit_thresholds
+    if underfit_threshold is None:
+        underfit_threshold = 50
+
+    # Definiere den Defaultwert des overfit_threshold
+    if overfit_threshold is None:
+        overfit_threshold = 5
+
+    # Geben die beiden Werte aus
+    print(f"accuracy_train: {as_train}%; accuracy_test: {as_test}%")
+
+    # Entscheide zw. Under- und Overfit
+    if as_train < underfit_threshold and as_test < underfit_threshold:
+        fit = f"Model is underfit"
+        threshold = underfit_threshold
+    else:
+        fit = "Model is overfit" if as_train - as_test > overfit_threshold else "Model generalizes well to unseen data"
+        threshold = overfit_threshold
+
+    # Geben das fit und den threshold zurueck
+    return fit, threshold
